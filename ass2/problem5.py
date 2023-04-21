fa75618fe49fca2351fb30e6011da6fd987e58ab
Van Giang <vangianganh@gmail.com>
2023-04-21 21:10:21 +0200

    finished prob5

--- ass2/problem5.py
+++ ass2/problem5.py
@@ -1,11 +1,33 @@
 import numpy as np
-from sklearn.preprocessing import normalize
-from generate import GENERATE
-import random
 import codecs
-
+from sklearn.preprocessing import normalize
 
 vocab = codecs.open("brown_vocab_100.txt")
 
-#load the indices dictionary
 word_index_dict = {k.rstrip(): v for v, k in enumerate(vocab.read().splitlines())}
+counts = np.zeros((len(word_index_dict), len(word_index_dict), len(word_index_dict)))# + 0.1
+
+f = codecs.open("brown_100.txt")
+for l in f.read().splitlines():
+    indices = np.apply_along_axis(lambda x: np.array([word_index_dict[i.lower()] for i in x]), 1, 
+                                  np.lib.stride_tricks.sliding_window_view(np.array(l.split()), 3))
+    # print(indices)
+    np.add.at(counts, (indices[:, 0], indices[:, 1], indices[:, 2]), 1)
+
+probs = counts / counts.sum(axis=2)[:,:, np.newaxis]
+# probs = probs.reshape(len(word_index_dict), len(word_index_dict), len(word_index_dict))
+
+spec_ind = np.array([
+    [word_index_dict["in"], word_index_dict["the"], word_index_dict["past"]],
+    [word_index_dict["in"], word_index_dict["the"], word_index_dict["time"]],
+    [word_index_dict["the"], word_index_dict["jury"], word_index_dict["recommended"]],
+    [word_index_dict["jury"], word_index_dict["said"], word_index_dict["that"],],
+    [word_index_dict["agriculture"], word_index_dict["teacher"], word_index_dict[","]],
+])
+
+spec_probs = probs[spec_ind[:, 0], spec_ind[:, 1], spec_ind[:, 2]]
+print(spec_probs)
+
+
+
+f.close()
